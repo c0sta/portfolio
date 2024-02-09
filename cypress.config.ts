5dbbd4ccfa4167ea99a1104720ffc02fdcf6470e
Gabriel Costa Moura <drt69727@MAC452093-M.local>
2024-02-09 11:47:57 -0300

    tests: added Cypress config

---
+++ cypress.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  projectId: "afskiz",
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+  },
+});
