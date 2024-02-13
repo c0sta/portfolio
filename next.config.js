beeab1ed2911e705a9db64e79f12954a9d1b7ce0
Gabriel Costa Moura <drt69727@MAC452093-M.local>
2024-02-13 10:02:10 -0300

    ci: added prod host on remotePatterns

--- next.config.js
+++ next.config.js
@@ -7,6 +7,12 @@ const nextConfig = {
         hostname: '127.0.0.1',
         port: '1337',
         pathname: '/uploads/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'portfolio-cms-production-5920.up.railway.app',
+        port: '',
+        pathname: '/uploads/**'
       }
     ]
   }
